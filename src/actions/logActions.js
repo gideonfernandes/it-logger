6b2a67e9be69f2a9eb6bc97478568b18cad9ee67
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-27 23:19:24 -0300

    Add logReducer, logActions and types

---
+++ src/actions/logActions.js
@@ -0,0 +1,28 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+// Get logs from json-server action
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
+
+    const response = fetch('/logs');
+    const data = (await response).json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  };
+};
+
+// Set loading to true action
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
