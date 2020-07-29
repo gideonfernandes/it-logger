b9ee2f1921e183fbe9300af13485dc2416f98ce6
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-07-28 21:51:52 -0300

    Add redux connection to EditLogModal component

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { updateLog } from '../../actions/logActions';
+import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() =>{
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    };
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Por favor, informe uma mensagem e um técnico.'})
     } else {
-      console.log(message, tech, attention);
+      const updatedLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      updateLog(updatedLog);
+      M.toast({ html: `Registro de ${tech} atualizado`});
 
       // Clear fields
       setMessage('');
@@ -31,12 +51,6 @@ const EditLogModal = () => {
               value={message}
               onChange={event => setMessage(event.target.value)}
             />
-            <label
-              htmlFor="message"
-              className="active"
-            >
-              Mensagem do registro
-            </label>
           </div>
         </div>
 
@@ -94,4 +108,13 @@ const modalStyle = {
   height: '75%'
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
